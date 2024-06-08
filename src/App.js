b94a64b7c814599d3a293506984dc8ac79f2c519
yinhang <sunyinhang@gmail.com>
2024-06-08 11:57:21 -0700

    antd-mobile and router configuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import CityList from "./pages/CityList";
+
+import { Button } from "antd-mobile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Project Root Component <Button>Login</Button> */}
+
+        {/* configure navigation menu */}
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/citylist">CityList</Link></li>
+        </ul>
+
+        {/* configure routers */}
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/citylist" element={<CityList />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
